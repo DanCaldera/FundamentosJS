0512093882da4e9c12e5d3b10e533551a3242a25
Daniel Caldera <bdcaldera@gmail.com>
2019-12-18 00:18:48 -0600

    clase1

--- clase1.js
+++ clase1.js
@@ -1,9 +1,9 @@
-var nombre = 'Sacha', apellido = 'Lifszyc'
-var edad = 28
+var nombre = 'Daniel', apellido = 'Caldera'
+var edad = 22
 
-edad = '28 años'
+edad = '22 años'
 
 console.log('Hola ' + nombre + ' ' + apellido)
 console.log('Tengo ' + edad)
 
-var peso = 75
+var peso = 70
