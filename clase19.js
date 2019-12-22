bfa0f7ae8b6ec9258ba325549d84853e90844241
Daniel Caldera <bdcaldera@gmail.com>
2019-12-21 22:14:30 -0600

    Clase 19

--- clase19.js
+++ clase19.js
@@ -1,7 +1,7 @@
-var sacha = {
-  nombre: 'Sacha',
-  apellido: 'Lifszyc',
-  altura: 1.72
+var daniel = {
+  nombre: 'Daniel',
+  apellido: 'Caldera',
+  altura: 1.8
 }
 
 var alan = {
@@ -36,7 +36,7 @@ var paula = {
 
 const esAlta = ({ altura }) => altura > 1.8
 
-var personas = [sacha, alan, martin, dario, vicky, paula]
+var personas = [daniel, alan, martin, dario, vicky, paula]
 
 var personasAltas = personas.filter(esAlta)
 // var personasAltas = personas.filter(function (persona) {
