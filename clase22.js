a4c1e783033b7426e8fd7f18b6413532e2c99a21
Daniel Caldera <bdcaldera@gmail.com>
2019-12-27 21:53:17 -0600

    Clase 22

--- clase22.js
+++ clase22.js
@@ -10,7 +10,7 @@ Persona.prototype.saludar = () => {
 
 Persona.prototype.soyAlto = () => this.altura > 1.8
 
-var sacha = new Persona('Sacha', 'Lifszyc', 1.72)
+var sacha = new Persona('Daniel', 'Caldera', 1.81)
 var erika = new Persona('Erika', 'Luna', 1.65)
 var arturo = new Persona('Arturo', 'Martinez', 1.89)
 
